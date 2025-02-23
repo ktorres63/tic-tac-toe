b22755729e2219eeb247fcc0d258e91c0019e523
ktorres63 <ktorresaro@unsa.edu.pe>
2025-02-22 22:53:22 -0500

    Finish checkWin function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Factory function to players
 
-function player(name, marker){
+function Player(name, marker){
   return {name, marker}
 }
 
@@ -27,22 +27,59 @@ const gameBoard = (function () {
       "", "", ""] 
   }
 
-  const isCellEmpty = (position) => board[position] === " ";
+  const isCellEmpty = (position) => board[position] === "";
 
-  return {printBoard, update, getBoard, isCellEmpty, reset}
+  const checkWin = () =>{
+    const winningCombinations = [
+      [0,1,2],[3,4,5],[6,7,8], // Filas
+      [0,3,6],[1,4,7],[2,5,8], //Columnas
+      [0,4,8],[2,4,6] // Diagonales
+    ]
+
+    for(let i = 0; i< winningCombinations.length; i++){
+      let combo = winningCombinations[i];
+
+      let pos1 = combo[0];
+      let pos2 = combo[1];
+      let pos3 = combo[2];
+
+      if(board[pos1] && board[pos1] === board[pos2] && board[pos1] === board[pos3]){
+        return board[pos1];
+      }
+    }
+    return null;
+  }
+
+
+
+  return {printBoard, update, getBoard, isCellEmpty, reset, checkWin}
 
 
 }) ();
 
+
+const gameController = (function () {
+  const player1 = Player("human", "X")
+  const player2 = Player("Computer", "O")
+
+})();
+
 gameBoard.update(0,"X")
-gameBoard.update(3,"O")
-gameBoard.printBoard()
-console.log(gameBoard.isCellEmpty(1));
-console.log(gameBoard.isCellEmpty(2));
-console.log(gameBoard.isCellEmpty(3));
-gameBoard.reset();
+console.log(gameBoard.checkWin());
+
+gameBoard.update(1,"O")
+console.log(gameBoard.checkWin());
+
+gameBoard.update(3,"X")
+console.log(gameBoard.checkWin());
+
+gameBoard.update(8,"O")
+console.log(gameBoard.checkWin());
+
+
+gameBoard.update(6,"X")
+console.log(gameBoard.checkWin());
+
 gameBoard.printBoard()
 
-console.log(gameBoard.isCellEmpty(1));
-console.log(gameBoard.isCellEmpty(2));
-console.log(gameBoard.isCellEmpty(3));
+
