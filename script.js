6ab707e07c571cf325a456d6ccf73ef02ff01b4e
ktorres63 <ktorresaro@unsa.edu.pe>
2025-02-23 21:57:10 -0500

    Finish Tic tac toe in console

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ const gameBoard = (function () {
   }
 
   const isCellEmpty = (position) => board[position] === "";
-
+  const checkTie = () =>{
+    return board.every(cell => cell!="") && !checkWin()
+  }
   const checkWin = () =>{
     const winningCombinations = [
       [0,1,2],[3,4,5],[6,7,8], // Filas
@@ -50,9 +52,7 @@ const gameBoard = (function () {
     return null;
   }
 
-
-
-  return {printBoard, update, getBoard, isCellEmpty, reset, checkWin}
+  return {printBoard, update, getBoard, isCellEmpty, reset, checkWin, checkTie}
 
 
 }) ();
@@ -62,24 +62,47 @@ const gameController = (function () {
   const player1 = Player("human", "X")
   const player2 = Player("Computer", "O")
 
+  let currentPlayer = player1;
+
+  const switchTurn = () =>{
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+  const playTurn = (position) =>{
+    if(gameBoard.isCellEmpty(position)){
+      gameBoard.update(position, currentPlayer.marker);
+      if(gameBoard.checkWin()){
+        console.log(`${currentPlayer.name} wins!`);
+        gameBoard.reset();
+        return;
+        // reiniciar
+      }
+      if(gameBoard.checkTie()){
+        console.log("It's a tie");
+        gameBoard.reset();
+        return;
+      }
+      switchTurn(); // Cambiar al siguiente jugador
+
+    }
+    else{
+      console.log("cell is already taken");
+    }
+  }
+  return {playTurn}
 })();
 
-gameBoard.update(0,"X")
-console.log(gameBoard.checkWin());
+// Simulación de un juego
+// Simulación de un juego
+gameController.playTurn(0); // Humano juega en la posición 0
+gameBoard.printBoard();
+gameController.playTurn(4); // Computadora juega en la posición 4
+gameBoard.printBoard();
+gameController.playTurn(1); // Humano juega en la posición 1
+gameBoard.printBoard();
+gameController.playTurn(5); // Computadora juega en la posición 5
+gameBoard.printBoard();
+gameController.playTurn(2); // Humano juega en la posición 2 (gana)
 
-gameBoard.update(1,"O")
-console.log(gameBoard.checkWin());
-
-gameBoard.update(3,"X")
-console.log(gameBoard.checkWin());
-
-gameBoard.update(8,"O")
-console.log(gameBoard.checkWin());
-
-
-gameBoard.update(6,"X")
-console.log(gameBoard.checkWin());
-
-gameBoard.printBoard()
-
-
+// Imprimir el tablero después de cada movimiento
+gameBoard.printBoard();
